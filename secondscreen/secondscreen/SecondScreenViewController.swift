cf12fc17994476025b8dd0b35436621923729cea
K Kollmann <git@k.kollmann.moe>
2015-11-10 01:39:02 +0100

    add 2nd viewcontroller

--- secondscreen/secondscreen/SecondScreenViewController.swift
+++ secondscreen/secondscreen/SecondScreenViewController.swift
@@ -1,21 +1,40 @@
 //
-//  SecondViewController.swift
+//  SecondScreenViewController.swift
 //  secondscreen
 //
-//  Created by Kay on 29/11/15.
+//  Created by Kay on 30/11/15.
 //  Copyright © 2015 KK. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondScreenViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var input = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        // change corner radius of label
+        displayLabel.clipsToBounds = true
+        displayLabel.layer.cornerRadius = 20
 
+        if let numberInput = Int(input) {
+            if (numberInput == 1 || numberInput == 0) {
+                print(input)
+                displayLabel.text = "\(input) it is..."
+            } else {
+                print(input)
+                displayLabel.text = "nope"
+            }
+        } else {
+            print(input)
+            displayLabel.text = "uh-nuh"
+        }
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
